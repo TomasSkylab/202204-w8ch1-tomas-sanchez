b9c247bd94962c0a4150348332aef62a627e353c
Tomas Sanchez <tms90@outlook.es>
2022-05-26 01:10:16 +0200

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import StarComponent from "./components/StarComponent";
+
+function App() {
+  return (
+    <>
+      <StarComponent />
+    </>
+  );
+}
+
+export default App;
